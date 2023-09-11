2570d1454ecaccb7227ca69bfedfed5944d79ff2
julienco03 <julian160903@gmail.com>
2023-09-11 19:21:56 +0200

    Change lining of pdf template

--- python/module02_python_intermediate/app3-pdf-template/main.py
+++ python/module02_python_intermediate/app3-pdf-template/main.py
@@ -8,10 +8,12 @@ df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # Set the header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    for y in range(22, 290, 10):
+    for y in range(20, 290, 10):
         pdf.line(10, y, 200, y)
 
     # Set the footer
@@ -24,9 +26,12 @@ for index, row in df.iterrows():
         pdf.add_page()
 
         # Set the footer
-        pdf.ln(265)
+        pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
