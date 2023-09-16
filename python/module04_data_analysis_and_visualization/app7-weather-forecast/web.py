ef7b6c2ab8fa6ffb1c626ac14056354d370c95bc
julienco03 <julian160903@gmail.com>
2023-09-16 19:45:28 +0200

    Get data from openweathermap api

--- python/module04_data_analysis_and_visualization/app7-weather-forecast/web.py
+++ python/module04_data_analysis_and_visualization/app7-weather-forecast/web.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast For The Next Days")
 
@@ -12,13 +13,6 @@ option = st.selectbox("Select data to view", ["Temperature", "Sky"])
 
 st.subheader(f"{option} for the next {days if days > 1 else ''} day{'s' if days > 1 else ''} in {location}")
 
-
-def get_data(forecasted_days):
-    dates_data = ["2022-01-01", "2022-05-27", "2022-10-17"]
-    temperatures_data = [10, 11, 14]
-    return dates_data, temperatures_data
-
-
-dates, temperatures = get_data(days)
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures"})
+data = get_data(location, days, option)
+figure = px.line(x=data[0], y=data[1], labels={"x": "Date", "y": "Temperatures"})
 st.plotly_chart(figure)
