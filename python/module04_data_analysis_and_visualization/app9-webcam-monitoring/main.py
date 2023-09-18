a021c7a49db914514801611e4e6d635a575b172b
julienco03 <julian160903@gmail.com>
2023-09-18 13:52:29 +0200

    Implement threading and finish app9

--- python/module04_data_analysis_and_visualization/app9-webcam-monitoring/main.py
+++ python/module04_data_analysis_and_visualization/app9-webcam-monitoring/main.py
@@ -1,12 +1,26 @@
 import cv2
 import time
+import glob
+import os
+from send_mail import send_mail
+from threading import Thread
+
+
+def clean_images_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -15,18 +29,36 @@ while True:
         first_frame = gray_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, iterations=2)
     cv2.imshow("My video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     for contour in contours:
         if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+    if status_list[0] == 1 and status_list[1] == 0:
+        email_thread = Thread(target=send_mail, args=(image_with_object, ))
+        email_thread.daemon = True
+        email_thread.start()
+
+        email_thread.join()
+
+        clean_thread = Thread(target=clean_images_folder)
+        clean_thread.daemon = True
+        clean_thread.start()
 
     key = cv2.waitKey(1)
     if key == ord("q"):
