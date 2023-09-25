c1927e5dd847aa55d2b16e7d32b69a85130a6ed5
julienco03 <julian160903@gmail.com>
2023-09-25 16:12:31 +0200

    Create database with sqlalchemy

--- python/module08_flask_django/app16-job-application-flask/app.py
+++ python/module08_flask_django/app16-job-application-flask/app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    email = db.Column(db.String(50))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(30))
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
-app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # if data.db does not exist, it will be created
+        app.run(debug=True, port=5000)
