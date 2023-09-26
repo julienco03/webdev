90e8a3dce41dc50dd2c9049ae29a682478c8c5b6
julienco03 <julian160903@gmail.com>
2023-09-26 17:42:50 +0200

    Store data in database

--- python/module08_flask_django/app17-job-application-django/jobapplication/models.py
+++ python/module08_flask_django/app17-job-application-django/jobapplication/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Form(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    mail = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50)
     date = models.DateField()
     occupation = models.CharField(max_length=20)
 
